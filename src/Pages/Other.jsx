0504f31abf4f3a5e73bdb64fcd47c07e47eb16cc
R0kyzas <97044639+R0kyzas@users.noreply.github.com>
2022-10-21 21:13:29 +0300

    Update Other.jsx

--- src/Pages/Other.jsx
+++ src/Pages/Other.jsx
@@ -12,7 +12,7 @@ const Other = (props) => {
 
     useEffect(() => {
         const getTickets = async () => {
-            const {data: res} = await axios.get('https://jellyfish-app-nxtf7.ondigitalocean.app:8080/app/api/lottery-tickets');
+            const {data: res} = await axios.get('http://139.59.202.149:90/api/lottery-tickets');
             setTicket(res.ticket);
         }
         trackPromise(getTickets());
